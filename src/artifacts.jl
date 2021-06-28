71f0d4c7c04bbb2742bcbe9c4b14bd3490d101b2
AdarshKumar712 <adarshkumar712.ak@gmail.com>
2021-06-28 00:40:55 -0400

    Update artifacts.toml

--- src/artifacts.jl
+++ src/artifacts.jl
@@ -1,12 +1,29 @@
-using Pkg.Artifacts
+using Pkg.GitTools
 
-const ARTIFACTS_TOML = joinpath(@__DIR__, "Artifacts.toml")
+isurl(path::AbstractString) = startswith(path, "http://") || startswith(path, "https://")
 
 function get_registered_file(name)
     global ARTIFACTS_TOML
     hash = artifact_hash(name, ARTIFACTS_TOML)
     isnothing(hash) && error("$name not registered.")
+    if !artifact_exists(hash)
+        println("Artifact doesn't exist. Downloading....")
+        get_artifact(name)
+        hash = artifact_hash(name, ARTIFACTS_TOML)
+    end
     artifact_path(hash)
+end
+
+function get_artifact(name)
+    meta = Artifacts.artifact_meta(name, ARTIFACTS_TOML)
+    file_path = meta["download"][1]["url"]
+    if isurl(file_path)
+        ensure_artifact_installed(name, ARTIFACTS_TOML)
+    else
+        file_name = split(file_path, "/")[end]
+        path = joinpath(@__DIR__, file_path[1:end-length(file_name)])
+        register_custom_file(name, file_name, path)
+    end
 end
 
 """
@@ -22,12 +39,12 @@ function register_custom_file(artifact_name, file_name, path)
     end
    
     file_hash = create_artifact() do artifact_dir
-        filegetter(joinpath(artifact_dir, file_name))
+        tarball = filegetter(joinpath(artifact_dir, file_name))
+        global tarball_hash = bytes2hex(GitTools.blob_hash(tarball))
     end
 
-    # register if no in artifact.toml
+    # register if not in artifact.toml
     if isnothing(artifact_hash(artifact_name, ARTIFACTS_TOML))
-        bind_artifact!(ARTIFACTS_TOML, artifact_name, file_hash)
+        bind_artifact!(ARTIFACTS_TOML, artifact_name, file_hash; download_info=[(file_path, tarball_hash)])
     end
 end
-
