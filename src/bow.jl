eccbf49baed237ffbc2eaa71a45e1ca54c4870e9
AdarshKumar712 <adarshkumar712.ak@gmail.com>
2021-07-25 12:45:46 -0400

    Add docstrings to bow functions

--- src/bow.jl
+++ src/bow.jl
@@ -1,5 +1,16 @@
 using HTTP
 
+"""
+    get_bow_indices(bow_key_or_path_list::Vector{String}, tokenizer)
+
+Returns a list of `list of indices` of words from each Bag of word in the `bow_key_or_path_list`, after tokenization. The functions looks for provided BoW key in the registered artifacts `Artifacts.toml` file. In case not present there, function expects that bow_key is provided as the complete path to the file the URL to download .txt file.
+
+# Example
+``` julia
+get_bow_indices(["legal", "military"])
+```
+
+"""
 function get_bow_indices(bow_key_or_path_list::Vector{String}, tokenizer)
     bow_indices = []
     bow_words = []
@@ -26,6 +37,14 @@ function get_bow_indices(bow_key_or_path_list::Vector{String}, tokenizer)
     return bow_indices, bow_words
 end
 
+"""
+    build_bow_ohe(bow_indices, tokenizer)
+
+Build and return a list of `one_hot_matrix` for each Bag Of Words list from indices. Each item of the list is of dimension `(num_of_BoW_words, tokenizer.vocab_size)`. 
+
+Note: While building the OHE of word indices, it only keeps those words, which have length `1` after tokenization and discard the rest.
+
+"""
 function build_bow_ohe(bow_indices, tokenizer)
     global device
     if bow_indices==nothing
@@ -46,5 +65,3 @@ function build_bow_ohe(bow_indices, tokenizer)
     onehot_single = onehot_single |> device
     return onehot_single, onehot
 end
-
-
